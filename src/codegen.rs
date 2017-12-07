8899b47bee6286704533bff474cbca5385c921f8
Yaron Tausky <yaron.tausky@gmail.com>
2017-12-07 22:24:52 +0100

    Encode halt instruction

--- src/codegen.rs
+++ src/codegen.rs
@@ -3,10 +3,14 @@ use ast;
 
 #[cfg(test)]
 fn generate_code<F: FnMut(u8)>(ast_node: &ast::EmitBytes, mut sink: F) {
-    if ast_node.mnemonic == "stop" {
-        sink(0x10)
-    };
-    sink(0x00)
+    if ast_node.mnemonic == "halt" {
+        sink(0x76)
+    } else {
+        if ast_node.mnemonic == "stop" {
+            sink(0x10)
+        };
+        sink(0x00)
+    }
 }
 
 #[cfg(test)]
@@ -27,5 +31,13 @@ mod tests {
         let mut code = vec![];
         generate_code(&ast, |byte| code.push(byte));
         assert_eq!(code, [0x10, 0x00])
+    }
+
+    #[test]
+    fn encode_halt() {
+        let ast = ast::EmitBytes::new("halt", &[]);
+        let mut code = vec![];
+        generate_code(&ast, |byte| code.push(byte));
+        assert_eq!(code, [0x76])
     }
 }
