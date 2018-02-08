50f1fac10c8967b6511383ca25c4414bef880f28
Yaron Tausky <yaron.tausky@gmail.com>
2018-02-08 20:05:38 +0100

    Add #[cfg(test)] to make warnings go away

--- src/keyword.rs
+++ src/keyword.rs
@@ -12,10 +12,15 @@ pub enum Mnemonic {
 pub enum Register {
     A,
     B,
+    #[cfg(test)]
     C,
+    #[cfg(test)]
     D,
+    #[cfg(test)]
     E,
+    #[cfg(test)]
     H,
+    #[cfg(test)]
     L,
 }
 
