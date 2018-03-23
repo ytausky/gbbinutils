c9e17369e6c02c8ad4ea169421f2f23c9207561f
Yaron Tausky <yaron.tausky@gmail.com>
2018-03-23 19:15:09 +0100

    Remove some useless code

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,7 @@ mod semantics;
 mod syntax;
 mod token;
 
-pub struct AnalyzedSrc;
-
-pub fn analyze_file(name: &str) -> AnalyzedSrc {
+pub fn analyze_file(name: &str) {
     use std::io::prelude::*;
     let mut file = std::fs::File::open(name).unwrap();
     let mut src = String::new();
@@ -17,53 +15,10 @@ pub fn analyze_file(name: &str) -> AnalyzedSrc {
     let mut ast_builder = semantics::AstBuilder::new();
     parse::parse_src(lexer::Lexer::new(&src), &mut ast_builder);
     dump_ast(ast_builder.ast());
-    AnalyzedSrc {}
 }
 
 fn dump_ast(ast: &Vec<ast::AsmItem>) {
     for ref item in ast {
         println!("{:?}", item)
-    }
-}
-
-#[cfg(test)]
-struct FirstPass;
-
-#[cfg(test)]
-impl FirstPass {
-    #[cfg(test)]
-    fn new(_src: &str) -> FirstPass {
-        FirstPass {}
-    }
-
-    #[cfg(test)]
-    fn sections(&self) -> SectionIterator {
-        SectionIterator {}
-    }
-}
-
-#[cfg(test)]
-type Section = u8;
-
-#[cfg(test)]
-struct SectionIterator;
-
-#[cfg(test)]
-impl Iterator for SectionIterator {
-    type Item = Section;
-
-    fn next(&mut self) -> Option<Section> {
-        None
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn first_pass_on_empty_src() {
-        let first_pass = FirstPass::new("");
-        assert_eq!(first_pass.sections().next(), None);
     }
 }
