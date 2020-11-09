9ef5f54251c66e5bb5fdaa3b66f429181ac8c976
Yaron Tausky <ytausky@protonmail.com>
2020-11-09 03:08:34 +0100

    Make gbas take multiple source files

--- src/bin/gbas.rs
+++ src/bin/gbas.rs
@@ -4,25 +4,29 @@ use std::fs::File;
 use std::io::Write;
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() <= 1 {
+    let mut args = std::env::args();
+    args.next();
+    let args: Vec<String> = args.collect();
+    if args.len() <= 0 {
         println!("{}: error: no input files", args[0]);
         std::process::exit(1)
     }
-    let filename = &args[1];
     let mut diagnostics = |diagnostic| println!("{}", diagnostic);
     let mut config = Config {
         input: InputConfig::default(),
         diagnostics: DiagnosticsConfig::Output(&mut diagnostics),
     };
-    let object = {
-        let mut assembler = Assembler::new(&mut config);
-        assembler.assemble(filename)
-    };
-    if let Some(object) = object {
-        let mut linker = Linker::new(&mut config);
-        let program = linker.link(vec![object]).unwrap();
-        let mut rom_file = File::create(filename.to_owned() + ".o").unwrap();
-        rom_file.write_all(&program.into_rom()).unwrap()
-    }
+    let mut assembler = Assembler::new(&mut config);
+    let objects: Vec<_> = args
+        .iter()
+        .map(|path| {
+            assembler
+                .assemble(path)
+                .unwrap_or_else(|| std::process::exit(1))
+        })
+        .collect();
+    let mut linker = Linker::new(&mut config);
+    let program = linker.link(objects).unwrap();
+    let mut rom_file = File::create(args[0].to_owned() + ".o").unwrap();
+    rom_file.write_all(&program.into_rom()).unwrap()
 }
